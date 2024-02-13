3e50346963bbc91f0496eb9120549e274ed45208
jhuly ambrozini <jhulyamfebr@gmial.com>
2024-02-13 16:36:01 -0300

    config: configurando o typeorm no projeto

---
+++ 1707852801879-Tests.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Tests1707852801879 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
